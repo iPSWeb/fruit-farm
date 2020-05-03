9fc353b79b82723cd358aaeb07e728f0bef88ded
pligin <i@psweb.ru>
2020-05-03 15:46:24 +0300

    New Config

---
+++ classes/_class.config.php
@@ -0,0 +1,36 @@
+<?PHP
+
+/*
+ * Author: pligin
+ * Site: psweb.ru
+ * Telegram: t.me/pligin
+ */
+
+
+if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
+class config{
+    public $HostDB = 'localhost';
+    public $UserDB = '';
+    public $PassDB = '';
+    public $BaseDB = '';
+
+    public $SYSTEM_START_TIME = 1357338458;
+    public $VAL = 'Руб.';
+    public $autoSborActive = true;//true - включен автосбор, false - выключен автосбор
+    public $autoSborTime = '12:00';//время автоматического сбора
+    public $costWelcomeText = 0;//стоимость приветственного сообщения
+    public $accountWelcomeText = 'b';//Счет списания: b - с покупок, p - с выплат
+    # PAYEER настройки
+    public $AccountNumber = 'P111111';
+    public $apiId = '11111111';
+    public $apiKey = '11111111';
+
+    public $shopID = '11111111';
+    public $secretW = '11111111';
+    # Настройки для оплаты кредита
+    public $kredit_shopID = 'ShopID';
+    public $kredit_secretW = 'SecretKEY';
+    #PSCoin
+    public $PSCoinWallet = 'PSC-X2QN-WQL6-K96N-7ZGB9';
+    public $PSCoinSecret = '';
+}
