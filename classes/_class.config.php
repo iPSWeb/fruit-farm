0aa25d3e76ec462af693ca6abf523e03504119f8
pligin <i@psweb.ru>
2020-06-29 08:52:04 +0300

    Start integrate PDO

--- classes/_class.config.php
+++ classes/_class.config.php
@@ -10,15 +10,16 @@
 if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
 class config{
     public $HostDB = 'localhost';
-    public $UserDB = '';
-    public $PassDB = '';
-    public $BaseDB = '';
-
+    public $UserDB = 'user_1_ff';
+    public $PassDB = '9U8u7V8j';
+    public $BaseDB = 'user_1_ff';
+    public $CharsetDB = 'utf8';
+    
     public $SYSTEM_START_TIME = 1357338458;
     public $VAL = 'Руб.';
     public $autoSborActive = true;//true - включен автосбор, false - выключен автосбор
     public $autoSborTime = '12:00';//время автоматического сбора
-    public $costWelcomeText = 0;//стоимость приветственного сообщения
+    public $costWelcomeText = 1;//стоимость приветственного сообщения
     public $accountWelcomeText = 'b';//Счет списания: b - с покупок, p - с выплат
     # PAYEER настройки
     public $AccountNumber = 'P111111';
@@ -29,5 +30,5 @@ class config{
     public $secretW = '11111111';
     #PSCoin
     public $PSCoinWallet = 'PSC-X2QN-WQL6-K96N-7ZGB9';
-    public $PSCoinSecret = '';
+    public $PSCoinSecret = 'speak although wood pair strife breast claw thunder stupid unlike deadly either';
 }
