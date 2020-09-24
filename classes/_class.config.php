d7addf4086f1c0eb3608933bbdd6d7c5d4e859fe
pligin <i@psweb.ru>
2020-09-24 19:37:22 +0300

    Add bonus settings

--- classes/_class.config.php
+++ classes/_class.config.php
@@ -10,17 +10,21 @@
 if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
 class config{
     public $HostDB = 'localhost';
-    public $UserDB = 'user_1_ff';
-    public $PassDB = '9U8u7V8j';
-    public $BaseDB = 'user_1_ff';
+    public $UserDB = '';
+    public $PassDB = '';
+    public $BaseDB = '';
     public $CharsetDB = 'utf8';
     
     public $SYSTEM_START_TIME = 1357338458;
     public $VAL = 'Руб.';
+    #автоматический сбор продуктов
     public $autoSborActive = true;//true - включен автосбор, false - выключен автосбор
     public $autoSborTime = '12:00';//время автоматического сбора
+    #приветственное сообщение
     public $costWelcomeText = 1;//стоимость приветственного сообщения
     public $accountWelcomeText = 'b';//Счет списания: b - с покупок, p - с выплат
+    #настройка бонуса
+    public $frequencyBonus = 24;//периодичность получения бонуса в часах
     # PAYEER настройки
     public $AccountNumber = 'P111111';
     public $apiId = '11111111';
