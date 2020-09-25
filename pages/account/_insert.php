68d51bc83aa41feb503b2e905be6d668108f5a45
pligin <i@psweb.ru>
2020-09-25 22:33:30 +0300

    Add PDO into insert page

--- pages/account/_insert.php
+++ pages/account/_insert.php
@@ -1,20 +1,12 @@
 <?PHP
 if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
-$_OPTIMIZATION["title"] = "Аккаунт - Пополнение баланса";
+$_OPTIMIZATION['title'] = 'Пополнение баланса';
 ?>
 <div class="s-bk-lf">
-	<div class="acc-title">Пополнение баланса</div>
+	<div class="acc-title">{!TITLE!}</div>
 </div>
-<?PHP
-/*
-if($_SESSION["user_id"] != 1){
-echo "<center><b><font color = red>Технические работы</font></b></center>";
-return;
-}
-*/
-?>
 <div class="silver-bk"> 
-Курс игровой валюты: 1 рубль (<?=$config->VAL; ?>) = <?=$db_config["ser_per_wmr"]; ?> серебра.
+Курс игровой валюты: 1 рубль = <?=$db_config['ser_per_wmr']; ?> серебра.
 <p>Ввод средств позволяет автоматически приобрести игровое серебро с помощью различных платежных 
 систем: Yandex Деньги, банковских карт, SMS, терминалов, денежных переводов и т.д.</p>
 <p>Оплата и зачисление серебра на баланс производится в автоматическом режиме.</p> 
@@ -23,39 +15,46 @@ return;
 <?PHP
 /// db_payeer_insert
 if(isset($_POST['sum'])){
-$sum = round(floatval($_POST["sum"]),2);
-if($sum >= 1){
-# Заносим в БД
-$db->Query("INSERT INTO `db_payeer_insert` (`user_id`, `user`, `sum`, `date_add`,`description`) VALUES ('$user_id','$user_name','$sum','".time()."','Payeer')");
-$desc = base64_encode("Пополнение баланса на проекте ".$_SERVER["HTTP_HOST"]." пользователем ".$user_name);
-$m_shop = $config->shopID;
-$m_orderid = $db->LastInsert();
-$m_amount = number_format($sum, 2, ".", "");
-$m_curr = "RUB";
-$m_desc = $desc;
-$m_key = $config->secretW;
-$arHash = array(
- $m_shop,
- $m_orderid,
- $m_amount,
- $m_curr,
- $m_desc,
- $m_key
-);
-$sign = strtoupper(hash('sha256', implode(":", $arHash)));
-?>
-<center>
-<form method="GET" action="//payeer.com/api/merchant/m.php">
-	<input type="hidden" name="m_shop" value="<?=$config->shopID; ?>">
-	<input type="hidden" name="m_orderid" value="<?=$m_orderid; ?>">
-	<input type="hidden" name="m_amount" value="<?=number_format($sum, 2, ".", "")?>">
-	<input type="hidden" name="m_curr" value="RUB">
-	<input type="hidden" name="m_desc" value="<?=$desc; ?>">
-	<input type="hidden" name="m_sign" value="<?=$sign; ?>">
-	<input type="submit" name="m_process" value="Оплатить и получить серебро" />
-</form>
-</center>
-<div class="clr"></div>		
+    $amount = round(floatval($_POST['sum']),2);
+    if($amount >= 1){
+        # Заносим в БД
+        $result = $pdo->prepare("INSERT INTO `db_payeer_insert` (`user_id`, `user`, `sum`, `date_add`,`description`) VALUES (:user_id,:user_name,:amount,:time,:description)");
+        $result->execute(array(
+            'user_id' => $user_id,
+            'user_name'=>$user_name,
+            'amount'=>$amount,
+            'time'=>time(),
+            'description'=>'Payeer'
+        ));
+        $desc = base64_encode("Пополнение баланса на проекте ".$_SERVER["HTTP_HOST"]." пользователем ".$user_name);
+        $m_shop = $config->shopID;
+        $m_orderid = $pdo->lastInsertId();
+        $m_amount = number_format($amount, 2, '.', '');
+        $m_curr = 'RUB';
+        $m_desc = $desc;
+        $m_key = $config->secretW;
+        $arHash = array(
+            $m_shop,
+            $m_orderid,
+            $m_amount,
+            $m_curr,
+            $m_desc,
+            $m_key
+        );
+        $sign = strtoupper(hash('sha256', implode(":", $arHash)));
+        ?>
+        <center>
+            <form method="POST" action="//payeer.com/merchant/">
+                <input type="hidden" name="m_shop" value="<?=$config->shopID; ?>">
+                <input type="hidden" name="m_orderid" value="<?=$m_orderid; ?>">
+                <input type="hidden" name="m_amount" value="<?=$m_amount;?>">
+                <input type="hidden" name="m_curr" value="<?=$m_curr;?>">
+                <input type="hidden" name="m_desc" value="<?=$desc; ?>">
+                <input type="hidden" name="m_sign" value="<?=$sign; ?>">
+                <input type="submit" name="m_process" value="Оплатить и получить серебро" />
+            </form>
+        </center>
+    <div class="clr"></div>		
 </div>
 <?PHP
 return;
