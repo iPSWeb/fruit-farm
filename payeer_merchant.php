bb51bae5ef7c0d486432df95cc55eb11f46954dd
pligin <i@psweb.ru>
2020-09-29 20:46:51 +0300

    Added pdo to Payeer payment processor

--- payeer_merchant.php
+++ payeer_merchant.php
@@ -9,95 +9,121 @@
 
 //Проверка IP сервера оповещений Payeer
 if (!in_array($_SERVER['REMOTE_ADDR'], array('185.71.65.92', '185.71.65.189', '149.202.17.210'))) die('ERROR IP');
-if (isset($_POST['m_operation_id']) && isset($_POST['m_sign']))
-{
-# Автоподгрузка классов
-	function __autoload($name){ include('classes/_class.'.$name.'.php');}
-# Класс конфига 
-	$config = new config;
-	$m_key = $config->secretW;
-# Формируем массив для генерации подписи
-	$arHash = array(
-	$_POST['m_operation_id'],
-	$_POST['m_operation_ps'],
-	$_POST['m_operation_date'],
-	$_POST['m_operation_pay_date'],
-	$_POST['m_shop'],
-	$_POST['m_orderid'],
-	$_POST['m_amount'],
-	$_POST['m_curr'],
-	$_POST['m_desc'],
-	$_POST['m_status']
-	);
-# Если были переданы дополнительные параметры, то добавляем их вмассив
-	if (isset($_POST['m_params']))
-	{
+if (isset($_POST['m_operation_id']) && isset($_POST['m_sign'])){
+    # Автоподгрузка классов
+    function __autoload($name){ include('classes/_class.'.$name.'.php');}
+    # Класс конфига 
+    $config = new config;
+    $m_key = $config->secretW;
+    # Формируем массив для генерации подписи
+    $arHash = array(
+        $_POST['m_operation_id'],
+        $_POST['m_operation_ps'],
+        $_POST['m_operation_date'],
+        $_POST['m_operation_pay_date'],
+        $_POST['m_shop'],
+        $_POST['m_orderid'],
+        $_POST['m_amount'],
+        $_POST['m_curr'],
+        $_POST['m_desc'],
+        $_POST['m_status']
+    );
+    # Если были переданы дополнительные параметры, то добавляем их вмассив
+    if (isset($_POST['m_params'])){
 	$arHash[] = $_POST['m_params'];
-	}
-# Добавляем в массив секретный ключ
-	$arHash[] = $m_key;
-# Формируем подпись
-	$sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
-# Если подписи совпадают и статус платежа “Выполнен”
-	if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success')
-	{
+    }
+    # Добавляем в массив секретный ключ
+    $arHash[] = $m_key;
+    # Формируем подпись
+    $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
+    # Если подписи совпадают и статус платежа “Выполнен”
+    if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success'){
+        $txnId = $_POST['m_operation_id'];
+        $wallet = $_POST['client_account'];
+        $email = $_POST['client_email'];
+        $order_id = $_POST['m_orderid'];
 	# База данных
-		$db = new db($config->HostDB, $config->UserDB, $config->PassDB, $config->BaseDB);
-	# Функции
-		$func = new func;
+	include(BASE_DIR.'/inc/_connect.php');
 	# Информация о платеже из базы
-		$db->Query("SELECT * FROM db_payeer_insert WHERE id = ".$_POST['m_orderid']) or die($_POST['m_orderid'].'|error');
+        $result = $pdo->prepare("SELECT * FROM `db_payeer_insert` WHERE `id` = :id");
+        $result->execute(array('id'=>$order_id));
 	# Если в базе нет такого платежа, выдаем "Ошибка"
-		if($db->NumRows() == 0){ exit($_POST['m_orderid'].'|error');}
+        if($result->rowCount() == 0){ exit($order_id.'|error');}
 	# Массив информации о платеже	
-		$payeer_row = $db->FetchArray();
+        $row = $db->FetchArray();
 	# Если статус платежа 1 ('Выполнено'), возвращаем 'Выполненно'
-		if($payeer_row['status'] == 1){ exit($_POST['m_orderid'].'|success');}
+        if($row['status'] == 1){ exit($order_id.'|success');}
 	# Если сумма платежа в оповещении не равна сумме в базе
-		if($payeer_row['sum'] != $_POST['m_amount']){ exit($_POST['m_orderid'].'|error');}
+        if($row['sum'] != $_POST['m_amount']){ exit($order_id.'|error');}
 	# Сумма платежа		
-		$amount = $payeer_row['sum'];
+        $amount = $row['sum'];
 	# ID пользователя
-		$user_id = $payeer_row['user_id']; 
+        $user_id = $row['user_id']; 
 	# Настройки из базы
-		$db->Query("SELECT * FROM db_config WHERE id = '1' LIMIT 1");
-		$config_site = $db->FetchArray();
+        $result = $pdo->query("SELECT * FROM `db_config` WHERE `id` = '1' LIMIT 1");
+        $config_site = $result->fetch();
 	# Информация о пользователе и реферере
-		$db->Query("SELECT user, referer_id FROM db_users_a WHERE id = '".$user_id."' LIMIT 1");
-		$user_data = $db->FetchArray();
-		$user_name = $user_data['user'];
-		$refid = $user_data['referer_id'];
+        $result = $pdo->prepare("SELECT * FROM `db_users_a`, `db_users_b` WHERE `db_users_a`.`id` = `db_users_b`.`id` AND `db_users_a`.`id` = :user_id");
+        $result->execute(array('user_id'=>$user_id));
+        $user_data = $result->fetch();
+        $user_name = $user_data['user'];
+        $referer_id = $user_data['referer_id'];
 	# Зачисляем баланс
-		$serebro = sprintf("%.4f", floatval($config_site['ser_per_wmr'] * $amount) );
-		$db->Query("SELECT insert_sum FROM db_users_b WHERE id = '".$user_id."' LIMIT 1");
-		$insert_sum = $db->FetchRow();
+        $serebro = $config_site['ser_per_wmr'] * $amount;
 	# Бонус при первом пополнении
-		$serebro = intval($insert_sum == 0) ? ($serebro + ($serebro * 0.55) ) : $serebro;
+        $serebro = $user_data['insert_sum'] == 0 ? $serebro + $serebro * $config->bonusFirstInsert/100 : $serebro;
 	# Дерево/фрукт при пополнении на определенную сумму
-		$add_tree = ( $amount >= 500) ? 2 : 0;
+        $add_tree = ( $amount >= 500) ? 2 : 0;
 	# Отчисления рефералу
-		$to_referer = ($serebro * 0.10);
-                $db->Query("SELECT `refback` FROM `db_users_a` WHERE `id` = '$refid'");
-                $refback = $db->FetchRow();
-                if($refback > 0){
-                    $amount_refback = $to_referer*$refback/100;
-                    $db->Query("UPDATE `db_users_b` SET `money_p` = `money_p` + '$amount_refback' WHERE `id` = '$user_id'");//начисление рефбек: money_p - на счет для вывода,money_b - на счет для оплаты
-                    $to_referer -= $amount_refback;
-                }
+        $to_referer = $serebro * $config->toRefererFromInsert / 100;
+        $result = $pdo->prepare("SELECT `refback` FROM `db_users_a` WHERE `id` = :referer_id");
+        $result->execute(array('referer_id'=>$referer_id));
+        $refback = $result->fetchColumn();
+        if($refback > 0){
+            $amount_refback = $to_referer*$refback/100;
+            $result = $pdo->prepare("UPDATE `db_users_b` SET `money_p` = `money_p` + :amount_refback WHERE `id` = :user_id");//начисление рефбек: money_p - на счет для вывода,money_b - на счет для оплаты
+            $result->execute(array(
+                'amount_refback' => $amount_refback,
+                'user_id' => $user_id
+            ));
+            $to_referer -= $amount_refback;
+        }
 	# Зачисляем пользователю
-		$db->Query("UPDATE db_users_b SET money_b = money_b + '$serebro', e_t = e_t + '$add_tree', to_referer = to_referer + '$to_referer', last_sbor = '".time()."', insert_sum = insert_sum + '$amount' WHERE id = '$user_id'") or die ($_POST['m_orderid'].'|error');
-		$db->Query("UPDATE db_payeer_insert SET status = '1' WHERE id = '".$_POST['m_orderid']."'") or die(mysqli_error());
-	# Зачисляем средства рефереру и дерево
-		$add_tree_referer = ($insert_sum <= 0.01) ? ", a_t = a_t + 1" : "";
-		$db->Query("UPDATE db_users_b SET money_b = money_b + $to_referer, from_referals = from_referals + '$to_referer' $add_tree_referer WHERE id = '$refid'") or die(mysqli_error());
+        $result = $pdo->prepare("UPDATE `db_users_b` SET `money_b`=`money_b`+ :serebro,`to_referer`=`to_referer`+ :to_referer,`last_sbor` = :time,`insert_sum`=`insert_sum`+ :amount WHERE `id` = :user_id");
+        $result->execute(array(
+            'serebro' => $serebro,
+            'to_referer'=>$to_referer,
+            'time' => time(),
+            'amount' => $amount,
+            'user_id' => $user_id
+        ));
+        $result = $pdo->prepare("UPDATE `db_payeer_insert` SET `status` = :status WHERE `id` = :order_id");
+        $result->execute(array(
+            'status'=>1,
+            'order_id'=>$order_id
+        ));
+	# Зачисляем средства рефереру
+        $result = $pdo->prepare("UPDATE `db_users_b` SET `money_b`=`money_b`+ :to_referer,`from_referals`=`from_referals`+ :from_referals WHERE `id` = :referer_id");
+        $result->execute(array(
+            'to_referer' => $to_referer,
+            'from_referals' => $to_referer,
+            'referer_id' => $referer_id
+        ));
 	# Статистика пополнений
-		$da = time();
-		$dd = $da + 60*60*24*15;
-		$db->Query("INSERT INTO db_insert_money (user, user_id, money, serebro, date_add, date_del) VALUES ('$user_name','$user_id','$amount','$serebro','$da','$dd')") or die(mysqli_error());
+        $da = time();
+        $dd = $da + 60*60*24*15;
+        $result = $pdo->prepare("INSERT INTO db_insert_money (`user`,`user_id`,`money`,`serebro`,`date_add`,`date_del`) VALUES (:user_name,:user_id,:amount,:serebro,:da,:dd)");
+        $result->execute(array(
+            'user_name' => $user_name,
+            'user_id' => $user_id,
+            'amount' => $amount,
+            'serebro' => $serebro,
+            'da' => $da,
+            'dd' => $dd
+        ));
 	# Обновление статистики сайта
-		$db->Query("UPDATE db_stats SET all_insert = all_insert + '$amount' WHERE id = '1'") or die(mysqli_error());
-		exit($_POST['m_orderid'].'|success');
-	}
+        $pdo->query("UPDATE `db_stats` SET `all_insert` = `all_insert` + '$amount' WHERE `id` = '1'");
+        exit($_POST['m_orderid'].'|success');
+    }
 }
 exit($_POST['m_orderid'].'|error');
-?>
