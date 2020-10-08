a576fca8f16b281b56df900963902035ead9a649
pligin <i@psweb.ru>
2020-10-08 10:45:28 +0300

    Add PDO

--- pages/admin/_sender.php
+++ pages/admin/_sender.php
@@ -2,26 +2,35 @@
 if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
 ?>
 <div class="s-bk-lf">
-	<div class="acc-title">Рассылка пользователям</div>
+    <div class="acc-title">Рассылка пользователям</div>
 </div>
 <div class="silver-bk"><div class="clr"></div>	
 <center>
-	<a href = "/admin/sender" class="stn">Список рассылок</a> || <a href = "/admin/sender/add" class="stn">Добавить рассылку</a>
+    <a href = "/admin/sender" class="stn">Список рассылок</a> || <a href = "/admin/sender/add" class="stn">Добавить рассылку</a>
 </center>
 <BR />
 <?PHP
-if(isset($_POST["title"])){
-$title = strval($_POST["title"]);
-$mess = $func->TextClean($_POST["mess"]);
-	if(strlen($title) > 3){
-		if(strlen($mess) > 10){
-		$db->Query("INSERT INTO db_sender (name, mess, date_add) VALUES ('$title','$mess','".time()."')");
-		echo "<center><b>Рассылка поставлена в очередь на выполнение</b></center><BR />";
-		}else echo "<center><b>Сообщение должно быть больше 10 символов</b></center><BR />";
-	}else echo "<center><b>Заголовок должен быть больше 3х символов</b></center><BR />";
+if(isset($_POST['title'])){
+    $title = strval($_POST['title']);
+    $mess = $func->TextClean($_POST['mess']);
+    if(strlen($title) > 3){
+        if(strlen($mess) > 10){
+            $result=$pdo->prepare("INSERT INTO db_sender (name, mess, date_add) VALUES (:title,:mess,:time)");
+            $result->execute(array(
+                'title'=>$title,
+                'mess'=>$mess,
+                'time'=>time()
+            ));
+            echo '<center><b>Рассылка поставлена в очередь на выполнение</b></center><BR />';
+        }else{
+            echo '<center><b>Сообщение должно быть больше 10 символов</b></center><BR />';
+        }
+    }else{
+        echo '<center><b>Заголовок должен быть больше 3х символов</b></center><BR />';
+    }
 }
 # Добавление рассылки
-if(isset($_GET["add"])){
+if(isset($_GET['add'])){
 ?>
 <form action="" method="post">
 <table width="" border="0">
@@ -54,12 +63,13 @@ if(isset($_GET["add"])){
 return;
 }
 # Удаление
-if(isset($_POST["del"])){
-	$db->Query("DELETE FROM db_sender WHERE id = '".intval($_POST["del"])."'");	
-	echo "<center><b>Рассылка удалена</b></center><BR />";
+if(isset($_POST['del'])){
+    $result=$pdo->prepare("DELETE FROM db_sender WHERE id = :id");
+    $result->execute(array('id'=>intval($_POST['del'])));
+    echo '<center><b>Рассылка удалена</b></center><BR />';
 }
-$db->Query("SELECT * FROM db_sender ORDER BY id DESC");
-if($db->NumRows() > 0){
+$result=$pdo->query("SELECT * FROM db_sender ORDER BY id DESC");
+if($result->rowCount() > 0){
 ?>
 <table cellpadding='3' cellspacing='0' border='0' bordercolor='#336633' align='center' width="99%">
   <tr bgcolor="#efefef">
@@ -70,20 +80,20 @@ if($db->NumRows() > 0){
 	<td align="center" width="50" class="m-tb"><b>Удалить</b></td>
   </tr>
 <?PHP
-while($data = $db->FetchArray()){
+while($data = $result->fetch()){
 ?>
-	<tr>
-    <td align="center"><?=$data["id"]; ?></td>
-    <td align="center"><?=$data["name"]; ?></td>
-    <td align="center"><?=$data["sended"]; ?> шт.</td>
+    <tr>
+        <td align="center"><?=$data["id"]; ?></td>
+        <td align="center"><?=$data["name"]; ?></td>
+        <td align="center"><?=$data["sended"]; ?> шт.</td>
 	<td align="center"><?=$data["status"] == 0 ? "Отправка" : "Завершено"; ?></td>
 	<td align="center">
-		<form action="" method="post">
-			<input type="hidden" name="del" value="<?=$data["id"]; ?>" />
-			<input type="submit" value="Удалить" />
-		</form>
+            <form action="" method="post">
+                    <input type="hidden" name="del" value="<?=$data["id"]; ?>" />
+                    <input type="submit" value="Удалить" />
+            </form>
 	</td>
-  	</tr>
+    </tr>
 <?PHP
 }
 ?>
