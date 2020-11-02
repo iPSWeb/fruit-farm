47f394b2d12d965ccb7b252a3e04b46a7b9d2b77
pligin <i@psweb.ru>
2020-11-02 22:53:34 +0300

    Fix list users page

--- pages/_users.php
+++ pages/_users.php
@@ -1,17 +1,17 @@
 <?PHP
 if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
+$_OPTIMIZATION['title'] = 'Список пользователей';
 ?>
 <div class="s-bk-lf">
-	<div class="acc-title">Игроки</div>
+    <div class="acc-title">{!TITLE!}</div>
 </div>
 <div class="silver-bk">
-	<div class="clr"></div>
-	<?PHP
-	$_OPTIMIZATION["title"] = "Аккаунт - Список пользователей";
-	$num_p = (isset($_GET["page"]) AND intval($_GET["page"]) < 1000 AND intval($_GET["page"]) >= 1) ? (intval($_GET["page"]) -1) : 0;
-	$lim = $num_p * 100;
-	$db->Query("SELECT * FROM db_users_a ORDER BY id LIMIT {$lim}, 100");
-	if($db->NumRows() > 0){
+    <div class="clr"></div>
+    <?PHP
+    $num_p = (isset($_GET['page']) AND intval($_GET['page']) < 1000 AND intval($_GET['page']) >= 1) ? (intval($_GET['page']) -1) : 0;
+    $lim = $num_p * 100;
+    $result=$pdo->query("SELECT * FROM `db_users_a` ORDER BY `id` LIMIT {$lim}, 100");
+	if($result->rowCount() > 0){
 	?>
 	<table width="100%" border="0">
 		<tr bgcolor="#efefef">
@@ -20,12 +20,12 @@ if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
 			<td align="center">Email</td>
 		</tr>
 	<?PHP
-		while($data = $db->FetchArray()){
+		while($data = $result->fetch()){
 		?>
 		<tr class="htt">
 			<td align="center"><?=$data["id"]; ?></td>
 			<td align="center"><?=$data["user"]; ?></td>
-			<td align="center"><?=str_replace(substr($data["email"],2,3), '<font color="red">***</font>', $data["email"]); ?></td>
+			<td align="center"><?=str_replace(substr($data["email"],2,3), '<font color="red">***</font>', $data['email']); ?></td>
 		</tr>
 		<?PHP
 		}
@@ -33,14 +33,16 @@ if (!defined('PSWeb') || PSWeb !== true) { Header('Location: /404'); return; }
 	</table>
 	<BR />
 	<?PHP
-	}else echo "<center><b>На данной странице нет записей</b></center><BR />";
-	$db->Query("SELECT COUNT(*) FROM db_users_a");
-	$all_pages = $db->FetchRow();
+	}else{
+            echo "<center><b>На данной странице нет записей</b></center><BR />";
+        }
+	$result=$pdo->query("SELECT COUNT(*) FROM `db_users_a`");
+	$all_pages = $result->fetchColumn();
 	if($all_pages > 100){
-		$sort_b = (isset($_GET["sort"])) ? intval($_GET["sort"]) : 0;
+		$sort_b = (isset($_GET['sort'])) ? intval($_GET['sort']) : 0;
 		$nav = new navigator;
-		$page = (isset($_GET["page"]) AND intval($_GET["page"]) < 1000 AND intval($_GET["page"]) >= 1) ? (intval($_GET["page"])) : 1;
-		echo "<BR /><center>".$nav->Navigation(10, $page, ceil($all_pages / 100), "/users/"), "</center>";
+		$page = (isset($_GET["page"]) AND intval($_GET['page']) < 1000 AND intval($_GET['page']) >= 1) ? (intval($_GET['page'])) : 1;
+		echo '<BR /><center>'.$nav->Navigation(10, $page, ceil($all_pages / 100), '/users/'), '</center>';
 	}
 	?>
 	<div class="clr"></div>
